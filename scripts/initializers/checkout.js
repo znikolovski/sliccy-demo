09d320816ac44274f228afdb59bdbf8eb1bcb1dd
Zoran Nikolovski <nikolovs@adobe.com>
2026-05-21 14:09:50 +1000

    Set checkout dropin endpoint to commerce-endpoint (Magento)

--- scripts/initializers/checkout.js
+++ scripts/initializers/checkout.js
@@ -1,10 +1,11 @@
 import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
 import { initializers } from '@dropins/tools/initializer.js';
-import { initialize, setFetchGraphQlHeaders } from '@dropins/storefront-checkout/api.js';
+import { initialize, setEndpoint, setFetchGraphQlHeaders } from '@dropins/storefront-checkout/api.js';
 import { initializeDropin } from './index.js';
 import { fetchPlaceholders } from '../commerce.js';
 
 await initializeDropin(async () => {
+  setEndpoint(getConfigValue('commerce-endpoint'));
   setFetchGraphQlHeaders((prev) => ({ ...prev, ...getHeaders('checkout') }));
 
   const labels = await fetchPlaceholders('placeholders/checkout.json');
