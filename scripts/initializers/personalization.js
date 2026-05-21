f94abd9c65a7ffff11143a0a240abb8ad48ee9fd
Zoran Nikolovski <nikolovs@adobe.com>
2026-05-21 14:20:53 +1000

    Set personalization dropin endpoint to commerce-endpoint (Magento)

--- scripts/initializers/personalization.js
+++ scripts/initializers/personalization.js
@@ -1,9 +1,10 @@
-import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
+import { getConfigValue, getHeaders } from '@dropins/tools/lib/aem/configs.js';
 import { initializers } from '@dropins/tools/initializer.js';
-import { initialize, setFetchGraphQlHeaders } from '@dropins/storefront-personalization/api.js';
+import { initialize, setEndpoint, setFetchGraphQlHeaders } from '@dropins/storefront-personalization/api.js';
 import { initializeDropin } from './index.js';
 
 await initializeDropin(async () => {
+  setEndpoint(getConfigValue('commerce-endpoint'));
   setFetchGraphQlHeaders((prev) => ({ ...prev, ...getHeaders('cart') }));
   return initializers.mountImmediately(initialize, {});
 })();
