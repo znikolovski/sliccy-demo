b41e5b6a9c1be684389aed72dfd6723bb5559b23
Zoran Nikolovski <nikolovs@adobe.com>
2026-05-21 14:09:48 +1000

    Set cart dropin endpoint to commerce-endpoint (Magento)

--- scripts/initializers/cart.js
+++ scripts/initializers/cart.js
@@ -1,10 +1,11 @@
 import { getHeaders } from '@dropins/tools/lib/aem/configs.js';
 import { initializers } from '@dropins/tools/initializer.js';
-import { initialize, setFetchGraphQlHeaders } from '@dropins/storefront-cart/api.js';
+import { initialize, setEndpoint, setFetchGraphQlHeaders } from '@dropins/storefront-cart/api.js';
 import { initializeDropin } from './index.js';
 import { fetchPlaceholders } from '../commerce.js';
 
 await initializeDropin(async () => {
+  setEndpoint(getConfigValue('commerce-endpoint'));
   setFetchGraphQlHeaders((prev) => ({ ...prev, ...getHeaders('cart') }));
 
   const labels = await fetchPlaceholders('placeholders/cart.json');
